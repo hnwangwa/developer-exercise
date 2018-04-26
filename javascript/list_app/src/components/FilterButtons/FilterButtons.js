715e98d68744477788d7ecd706b27e0755a9212e
hnwangwa <hollynwangwa@gmail.com>
2018-04-25 23:36:18 -0500

    psuedo-coded attempt at filter buttons
    got as far as console logging quotes filtered on movies/games

---
+++ javascript/list_app/src/components/FilterButtons/FilterButtons.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+//I got as far as console logging the filtered quotes.
+class FilterButtons extends Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			isFiltered: false
+		};
+	}
+
+	toggleMovies = () => {
+		this.setState({
+			isFiltered: !this.state.isFiltered
+			});
+		var _ = require('lodash');
+ 		var quotes = this.props.quotes;
+ 		var filteredMovieQuotes = _.filter(quotes, {theme: "movies"});
+ 		console.log(filteredMovieQuotes);
+
+
+
+	}
+	toggleGames = () => {
+		this.setState({
+			isFiltered: !this.state.isFiltered
+			});
+		var _ = require('lodash');
+ 		var quotes = this.props.quotes;
+ 		var filteredGameQuotes = _.filter(quotes, {theme: "games"});
+ 		console.log(filteredGameQuotes);
+
+	}
+
+	render () {
+		return (
+			<div>
+				<ul>
+					<button onClick={ () => this.toggleMovies()}>Show me movie quotes!</button>
+					<button onClick={ () => this.toggleGames()}>Show me game quotes!</button>
+				</ul>
+			</div>
+		);
+	}
+};
+
+
+export default FilterButtons;
--- javascript/list_app/src/pages/Main/Main.js
+++ javascript/list_app/src/pages/Main/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./Main.css";
+//Component meant to handle filtering:
+//import FilterButtons from "../../components/FilterButtons";
+
 
 class Main extends Component {
 	constructor(props) {
@@ -17,31 +20,31 @@ class Main extends Component {
  	this.setState({
  		currentPage: Number(event.target.id)
  	});
- 	console.log(this.state);
  }
   
-  componentDidMount() {
-    fetch("https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json")
-      .then(res => {
-        	return res.json()
-        })
-      .then(res => {
-      		  console.log(res);
-          this.setState({
-          	isLoaded: true,
-            quotes: res
-          });
-     
-        }),
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-         
+ componentDidMount() {
+ 	fetch("https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json")
+ 	.then(res => {
+ 		return res.json()
+ 	})
+ 	.then(res => {
+ 		console.log(res);
+ 		this.setState({
+ 			isLoaded: true,
+ 			quotes: res
+ 		});
+ 
 
-  }
+ 	}),
+ 	(error) => {
+ 		this.setState({
+ 			isLoaded: true,
+ 			error
+ 		});
+ 	}
+
+
+ }
   render() {
   	const { error, isLoaded, quotes, currentPage, quotesPerPage } = this.state;
   	const indexOfLastQuote = currentPage * quotesPerPage;
@@ -67,23 +70,25 @@ class Main extends Component {
   			);
   	});
 
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-      	<div>
-      		<h1>Fun Quotes!</h1>
-        <ul>
-          {renderQuotes}
-        </ul>
-        <ul id="page-numbers">
-              {renderPageNumbers}
-            </ul>
-        </div>
-      );
-    }
+  	if (error) {
+  		return <div>Error: {error.message}</div>;
+  	} else if (!isLoaded) {
+  		return <div>Loading...</div>;
+  	} else {
+  		return (
+  			<div>
+  			<h1>Fun Quotes!</h1>
+		      	{/*Insert completed component to handle filter button here!
+		      		<FilterButtons quotes={this.state.quotes}/>*/}
+		      			<ul>
+		      				{renderQuotes}
+		      		</ul>
+		      			<ul id="page-numbers">
+		      			{renderPageNumbers}
+		      		</ul>
+			</div>
+		);
+  	}
   }
 }
 export default Main;
