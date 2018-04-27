323c71c9df58dbb47d716120b01737993390fae4
hnwangwa <hollynwangwa@gmail.com>
2018-04-27 15:12:45 -0500

    fixed 'suite' typo, tried adding more failing test cases for the Hand class. noted with pseudo code where I need to pick back up
    fixed 'suite' typo, tried adding more failing test cases for the Hand class. noted with pseudo code where I need to pick back up

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,14 +1,14 @@
 class Card
-  attr_accessor :suite, :name, :value
+  attr_accessor :suit, :name, :value
 
-  def initialize(suite, name, value)
-    @suite, @name, @value = suite, name, value
+  def initialize(suit, name, value)
+    @suit, @name, @value = suit, name, value
   end
 end
 
 class Deck
   attr_accessor :playable_cards
-  SUITES = [:hearts, :diamonds, :spades, :clubs]
+  SUITS = [:hearts, :diamonds, :spades, :clubs]
   NAME_VALUES = {
     :two   => 2,
     :three => 3,
@@ -35,9 +35,9 @@ class Deck
 
   def shuffle
     @playable_cards = []
-    SUITES.each do |suite|
+    SUITS.each do |suit|
       NAME_VALUES.each do |name, value|
-        @playable_cards << Card.new(suite, name, value)
+        @playable_cards << Card.new(suit, name, value)
       end
     end
   end
@@ -49,17 +49,98 @@ class Hand
   def initialize
     @cards = []
   end
+
+  def add_card(card)
+    @cards << card
+  end
+
+  def value
+    # Want to add up the values of the cards here, keeping in mind that aces have two values
+    @cards.each do |card|
+      # TODO
+    end
+  end
+
+  def busted?
+    # TODO
+  end
+
+  def blackjack?
+    # TODO
+  end
 end
 
+
 require 'test/unit'
+
+class HandTest < Test::Unit::TestCase
+  def setup
+    @hand = Hand.new
+  end
+
+  def test_add_card_adds_cards
+    card = Card.new(:hearts, :ten, 10)
+    @hand.add_card(card)
+    assert(@hand.cards.include?(card))
+  end
+
+  def test_busted_false_when_score_less_than_21
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    refute(@hand.busted?)
+  end
+
+  def test_busted_false_when_score_equal_21
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    @hand.add_card(Card.new(:hearts, :nine, 9))
+    @hand.add_card(Card.new(:hearts, :two, 2))
+    refute(@hand.busted?)
+  end
+
+  def test_busted_false_when_score_greater_than_21
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    @hand.add_card(Card.new(:hearts, :nine, 9))
+    @hand.add_card(Card.new(:hearts, :three, 3))
+    assert(@hand.busted?)
+  end
+
+  def test_value_when_simple
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    @hand.add_card(Card.new(:hearts, :nine, 5))
+    @hand.add_card(Card.new(:hearts, :nine, 3))
+    assert_equal @hand.value, 18
+  end
+
+  def test_value_when_over_21_with_ace
+    @hand.add_card(Card.new(:hearts, :ten, 10))
+    @hand.add_card(Card.new(:clubs, :five, 5))
+    @hand.add_card(Card.new(:clubs, :ace, [11, 1]))
+    assert_equal @hand.value, 16
+  end
+
+  def test_value_when_under_21_with_ace
+    @hand.add_card(Card.new(:hearts, :nine, 9))
+    @hand.add_card(Card.new(:clubs, :ace, [11, 1]))
+    assert_equal @hand.value, 20
+  end
+
+  def test_is_blackjack_when_ace_and_10_point_card
+    # TODO
+  end
+
+  def test_is_not_blackjack_when_ace_and_any_other_card
+    # TODO
+  end
+
+end
 
 class CardTest < Test::Unit::TestCase
   def setup
     @card = Card.new(:hearts, :ten, 10)
   end
-  
-  def test_card_suite_is_correct
-    assert_equal @card.suite, :hearts
+
+  def test_card_suit_is_correct
+    assert_equal @card.suit, :hearts
   end
 
   def test_card_name_is_correct
@@ -74,14 +155,14 @@ class DeckTest < Test::Unit::TestCase
   def setup
     @deck = Deck.new
   end
-  
+
   def test_new_deck_has_52_playable_cards
     assert_equal @deck.playable_cards.size, 52
   end
-  
+
   def test_dealt_card_should_not_be_included_in_playable_cards
     card = @deck.deal_card
-    assert(@deck.playable_cards.include?(card))
+    assert(!@deck.playable_cards.include?(card))
   end
 
   def test_shuffled_deck_has_52_playable_cards
